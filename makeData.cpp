4c43574e102f614a95af0549278d0d2a1643d337
Judy <paoc@umich.edu>
2018-11-28 18:46:07 -0500

    changed makeData so it actually generates the amount of data I want. It was originally stopping at 1000000, so nothing else was accurate

--- makeData.cpp
+++ makeData.cpp
@@ -8,23 +8,24 @@
 //./run <size of file>
 int main(int argc, char * argv[])
 {
-  int datasetSize = atoi(argv[1]);
-  int randomNumber;
-  std::set<int> set;
+  long long int datasetSize = atoi(argv[1]);
+  long long int randomNumber;
+  auto set = new std::set<long long int>();
 
   srand (time(NULL));
-  while (set.size() != datasetSize)
+  while (set->size() != datasetSize)
   {
     randomNumber = rand();
     if (randomNumber == 0)
     {
       continue;
     }
-    auto ret = set.insert(randomNumber);
+    auto ret = set->insert(randomNumber);
     if (ret.second == true)
     {
       std::cout << randomNumber << ",";
     }
   }
+  delete set;
   return 0;
 }
